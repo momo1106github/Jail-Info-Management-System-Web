edefd436abd571a8937ff5e21b5c8713c80aeb43
Brandon <manabran501@hotmail.com>
2021-10-07 14:32:39 -0700

    React create app

---
+++ jail-info-management-system-web/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header'
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello from a class</h1>
+  }
+}
+export default App;
