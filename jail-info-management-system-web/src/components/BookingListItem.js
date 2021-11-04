515e42dc480cb5d0567d90ed583339d3f041b1e6
Kyle <kylejinjones@gmail.com>
2021-11-03 22:20:35 -0700

    Added comments to booking list item

--- jail-info-management-system-web/src/components/BookingListItem.js
+++ jail-info-management-system-web/src/components/BookingListItem.js
@@ -1,3 +1,6 @@
+//Used to list out items in booking for lists
+//Generates a button by each item that can delete the specified item from the list with onClick
+
 const BookingListItem = ({key, label, onClick}) => {
     return (
         <div className= 'BookingListItem' key = {key}>
