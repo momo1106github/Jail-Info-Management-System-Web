882a742caa33186085e793c65052ebb8f5a27713
garciajrraul <rgarcia5558@sdsu.edu>
2021-11-15 09:40:29 -0800

    New Transaction Mostly Complete

---
+++ jail-info-management-system-web/src/components/Transactions/SafeTransactions.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+const SafeTransactions = () => {
+
+    return (
+        <div className = "SafeTransactions">
+            <h1>Safe Transaction</h1>
+        </div>
+      )
+  }
+  export default SafeTransactions
--- jail-info-management-system-web/src/components/Transactions/TransactionLog.js
+++ jail-info-management-system-web/src/components/Transactions/TransactionLog.js
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
 import { useState } from 'react';
-=======
-import Navbar from "../Navbar/Navbar";
-import Header from "../Header";
->>>>>>> a4a2d0b93276520564fdc0821131136ec26b3706
 
 const TransactionLog = () => {
 
     return (
         <div className = "Transactionlog">
-<<<<<<< HEAD
             <h1>Transaction Log</h1>          
-=======
-            <Navbar></Navbar>
-            <Header title='Trust Accounts: Transaction Log'/>
-            <h1>Work?</h1>
->>>>>>> a4a2d0b93276520564fdc0821131136ec26b3706
       </div>
       )
   }
